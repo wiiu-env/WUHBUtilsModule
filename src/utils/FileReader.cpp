d1064192e5abb469a938e65bcf1f95bb742bf246
Maschell <Maschell@gmx.de>
2022-04-27 19:42:25 +0200

    Improve open files tracking implementation

--- src/utils/FileReader.cpp
+++ src/utils/FileReader.cpp
@@ -24,6 +24,14 @@ int64_t FileReader::read(uint8_t *buffer, uint32_t size) {
     return -2;
 }
 
+FileReader::FileReader(uint8_t *buffer, uint32_t size) {
+    this->input_buffer     = buffer;
+    this->input_size       = size;
+    this->input_pos        = 0;
+    this->isReadFromBuffer = true;
+    this->isReadFromFile   = false;
+}
+
 FileReader::FileReader(std::string &path) {
     int fd;
     if ((fd = open(path.c_str(), O_RDONLY)) >= 0) {
@@ -41,10 +49,7 @@ FileReader::~FileReader() {
     }
 }
 
-FileReader::FileReader(uint8_t *buffer, uint32_t size) {
-    this->input_buffer     = buffer;
-    this->input_size       = size;
-    this->input_pos        = 0;
-    this->isReadFromBuffer = true;
-    this->isReadFromFile   = false;
+
+bool FileReader::isReady() {
+    return this->isReadFromFile || this->isReadFromBuffer;
 }
