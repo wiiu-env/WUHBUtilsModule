901ee17e6f809b80f71924fe081cd920acde5a79
Maschell <Maschell@gmx.de>
2023-07-19 19:13:25 +0200

    Log the module version on each application start

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,17 @@
 #include "export.h"
 #include "utils/logger.h"
+#include "version.h"
 #include <wums.h>
 
 WUMS_MODULE_EXPORT_NAME("homebrew_wuhb_utils");
 
 WUMS_USE_WUT_DEVOPTAB();
 
+#define MODULE_VERSION      "v0.1.1"
+#define MODULE_VERSION_FULL MODULE_VERSION MODULE_VERSION_EXTRA
+
 WUMS_APPLICATION_STARTS() {
+    OSReport("Running WUHBUtilsModule " MODULE_VERSION_FULL "\n");
     initLogging();
 }
 
